7aeb3400205c2302b96cc003f017ddd56e2a79a6
nryz <mail@nryz.xyz>
2023-06-11 15:22:54 +0100

    format rust flake template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -16,7 +16,13 @@
         in {
           default = with pkgs;
             mkShell {
-              buildInputs = [cargo rustc rustfmt rustPackages.clippy rust-analyzer];
+              buildInputs = [
+                cargo
+                rustc
+                rustfmt
+                rustPackages.clippy
+                rust-analyzer
+              ];
               RUST_SRC_PATH = rustPlatform.rustLibSrc;
             };
         }
